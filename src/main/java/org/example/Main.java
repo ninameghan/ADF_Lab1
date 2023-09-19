1154f2f069cd7bf248dc265feadb0c260a9edd22
lemmenn <nina.lemmens@hva.nl>
2023-09-19 14:22:28 +0200

    Created Spring beans using Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,13 @@ package org.example;
 
 import org.example.entities.Country;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Beans created using XML
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.config.xml");
         Country ireland = (Country) applicationContext.getBean("ie");
         Country spain = (Country) applicationContext.getBean("es");
@@ -17,5 +20,14 @@ public class Main {
         System.out.println(france);
         System.out.println(nigeria);
         System.out.println(egypt);
+
+        // Beans created using Java
+        ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(Beans.class);
+        Country brazil = (Country) applicationContext1.getBean("br");
+        Country peru = (Country) applicationContext1.getBean("pe");
+        Country venezuela = (Country) applicationContext1.getBean("ve");
+        System.out.println(brazil);
+        System.out.println(peru);
+        System.out.println(venezuela);
     }
 }
