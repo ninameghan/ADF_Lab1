3aab7e26ea8c92d4a043e3de9b1da244eb8c54fb
lemmenn <nina.lemmens@hva.nl>
2023-09-19 14:09:21 +0200

    Created Spring beans using XML

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.entities.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.config.xml");
+        Country ireland = (Country) applicationContext.getBean("ie");
+        Country spain = (Country) applicationContext.getBean("es");
+        Country france = (Country) applicationContext.getBean("fr");
+        Country nigeria = (Country) applicationContext.getBean("ni");
+        Country egypt = (Country) applicationContext.getBean("eg");
+        System.out.println(ireland);
+        System.out.println(spain);
+        System.out.println(france);
+        System.out.println(nigeria);
+        System.out.println(egypt);
     }
 }
---
+++ src/main/java/org/example/entities/Country.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+    private String name;
+    private String capitalCity;
+    private Double populationInMillions;
+}
