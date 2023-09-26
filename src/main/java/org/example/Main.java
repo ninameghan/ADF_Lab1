b8cb49993ff6cb277ec64005f0f7ec7c04ece292
lemmenn <nina.lemmens@hva.nl>
2023-09-26 17:12:16 +0200

    Lab 2 - Change Languages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,13 +14,17 @@ public class Main {
 
         // Beans created using XML
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.config.xml");
-        System.out.println("XML Beans\n===========");
+        System.out.println();
+        System.out.println(applicationContext.getMessage("xmlHeading", null, Locale.getDefault()));
+        System.out.println("====================");
         applicationContext.getBeansOfType(Country.class)
                 .forEach((key, value) -> System.out.println(key + ": " + value));
 
         // Beans created using Java
         ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(Beans.class);
-        System.out.println("\nJava Beans\n===========");
+        System.out.println();
+        System.out.println(applicationContext.getMessage("javaHeading", null, Locale.FRENCH));
+        System.out.println("====================");
         applicationContext1.getBeansOfType(Country.class)
                 .forEach((key, value) -> System.out.println(key + ": " + value));
     }
